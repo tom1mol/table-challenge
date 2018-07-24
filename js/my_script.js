41dc13d7e0eaf4e2fa6b80cf0e4ab97cb96bd197
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-07-24 22:51:45 +0000

    other traversing methods-challenge 2

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {       //inside this only runs once DOM fully loaded
     $("tr:odd").css("background-color", "#f06b0d");  
     $("tr:even").css("background-color", "#f0e1d7");
+    
+    
+    $("th").click(function(){
+        $("tr").children().removeClass("selection")
+        $(this).siblings().addClass("selection");
+    });
 });
